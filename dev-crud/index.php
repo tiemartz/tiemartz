74bd56613d8c39c3169e61f15e0e27afe7170029
tiemartz <jamboysupitran@gmail.com>
2024-06-20 14:20:55 +0800

    changing employee to registry

--- dev-crud/index.php
+++ dev-crud/index.php
@@ -29,7 +29,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Employees Details</h2>
+                        <h2 class="pull-left">registry</h2>
                         <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
                     </div>
                     <?php
