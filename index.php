7b85d85c6227b3995e9a34bc618d35383a0285de
tiemartz <jamboysupitran@gmail.com>
2024-06-24 13:32:02 +0800

    sad

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: dev-crud/dashboard.php");
+                            header("location: dev-crud/index.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
